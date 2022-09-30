75b4af76aea048acb547176eb3654c5781c38bf3
ponhodu <zzz3415@hanmail.net>
2022-09-30 17:47:20 +0900

    9/30 homework

---
+++ P20220930/src/co/edu/friends/Friends.java
@@ -0,0 +1,5 @@
+package co.edu.friends;
+
+public class Friends {
+
+}
---
+++ P20220930/src/co/edu/friends/FriendsPgm.java
@@ -0,0 +1,24 @@
+package co.edu.friends;
+
+public class FriendsPgm {
+	private int num;
+	private String name;
+	private String bday;
+	private String fav;
+	
+	void setNum(int num) {
+		this.num =num;
+	}
+	public int getNum() {
+		return this.num;
+	}
+	void setName(String name) {
+		this.num =num;
+	}
+	void setBday(String bday) {
+		this.bday = bday;
+	}
+	void setFav(String fav) {
+		this.fav = fav;
+	}
+}
