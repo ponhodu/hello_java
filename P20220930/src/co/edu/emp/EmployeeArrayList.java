02ca9ae18129958b3c837630c0f3447c44b6b337
뚀민 <@DESKTOP-I1U1PUT>
2022-10-05 00:44:03 +0900

    10/04 ArrayList 사용해서 EmployeeArrayList를 만들었다

--- P20220930/src/co/edu/emp/EmployeeArrayList.java
+++ P20220930/src/co/edu/emp/EmployeeArrayList.java
@@ -1,35 +1,60 @@
 package co.edu.emp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 //컬렉션 이용해 정보 저장.
 public class EmployeeArrayList implements EmployeeService {
-
+	
+	Employee[] list;
+	int idx;
+	Scanner scn = new Scanner(System.in);
+	ArrayList List = new ArrayList();
+	
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void input() {
-		// TODO Auto-generated method stub
+		if(idx >=list.length) {
+			System.out.println("더 이상 입력 불가");
+			return;
+		}
+		System.out.println("사번 >>");
+		int eId = Integer.parseInt(scn.nextLine());
+		System.out.println("이름 >>");
+		String name = scn.nextLine();
+		System.out.println("부서 >>");
+		int deptId = Integer.parseInt(scn.nextLine());
+		System.out.println("급여 >> ");
+		int sal = Integer.parseInt(scn.nextLine());
+		System.out.println("이메일 >>");
+		String email = scn.nextLine();
 		
+		Employee emp = new Employee(eId, name, deptId, sal, email);
+		List.add(emp);
 	}
 
 	@Override
 	public void search() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public String search(int employeeId) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		for(int i =0; i<List.size(); i++) {
+			
+		}
 		
 	}
 
